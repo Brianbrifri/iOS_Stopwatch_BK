6aac0165bfb759f1915d252824672d3d2d688024
vm mac <vmmac@vms-mac.local>
2016-09-10 13:36:29 -0500

    Timer functionality works

--- Stopwatch_BK/Stopwatch_BK/ViewController.swift
+++ Stopwatch_BK/Stopwatch_BK/ViewController.swift
@@ -8,22 +8,51 @@ class ViewController: UIViewController {
     let PAUSED: Int = 2
     
     var currentTime = 0
+    var lapTime = 0
+    var secondsPortion = 0
+    var millisecondsPortion = 0
     var timer = Timer()
-    var stopWatchState = 0
+    var stopWatchState: Int = 0
     
+    @IBOutlet weak var stopWatchTimerDisplay: UILabel!
     @IBOutlet weak var stopWatchLapResetButton: UIButton!
     @IBAction func lapResetTouchUpInside(_ sender: AnyObject) {
-        
+        if stopWatchState == PAUSED {
+            currentTime = 0
+            self.stopWatchTimerDisplay.text = "00:00"
+            self.stopWatchLapResetButton.setTitle("Lap", for: .normal)
+        }
+        else {
+            self.lapTime = 0
+        }
     }
     
     @IBOutlet weak var stopWatchStartStopButton: UIButton!
-    @IBAction func startStopTouchUpInside(_ sender: AnyObject) {
-        if stopWatchState == STOPPED {
+    @IBAction func Starter(_ sender: AnyObject) {
+        print("Start Stop Pressed")
+        if stopWatchState == STOPPED || stopWatchState == PAUSED {
+            stopWatchState = RUNNING
+            self.stopWatchLapResetButton.setTitle("Lap", for: .normal)
+            self.stopWatchStartStopButton.setTitle("Stop", for: .normal)
+            timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (Timer) in
+                self.currentTime = self.currentTime + 1
+                self.lapTime = self.lapTime + 1
+                print("\(self.lapTime)")
+                self.secondsPortion = self.currentTime / 100
+                self.millisecondsPortion = self.currentTime % 100
+                self.stopWatchTimerDisplay.text = "\(self.secondsPortion):\(self.millisecondsPortion)"
+            })
+        }
+        else {
+            self.stopWatchStartStopButton.setTitle("Start", for: .normal)
+            self.stopWatchLapResetButton.setTitle("Reset", for: .normal)
+            stopWatchState = PAUSED
             timer.invalidate()
         }
     }
 
-    @IBOutlet weak var stopWatchTimerDisplay: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
